1b5c546041bc329d191eabd4dd00490933230956
MD Toushif Alam <136896060+ToushifAlam@users.noreply.github.com>
2025-07-07 17:15:08 +0530

    Update Navbar.tsx

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -125,7 +125,7 @@ function Navbar() {
                 delay: 0.6,
               }}
             >
-                <Button text="Resume" link="http://localhost:3000/resume.pdf" />
+                <Button text="Resume" link="https://portfolio-app-sooty-pi.vercel.app/resume.pdf" />
             </motion.div>
         </div>
     </div>
